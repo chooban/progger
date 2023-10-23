14ce20930303cd5244ce42482d2a0e2c2670e431
Ross Hendry <rhendry@googlemail.com>
2023-10-23 22:00:44 +0100

    aider: Added a test function `TestGetProgNumber` in `internal/scanner/scanner_test.go` to test the `getProgNumber` function with different file names and check if it correctly extracts the prog number from the file name.
    # Aider chat conversation:
    
    ASSISTANT: To add a test for the `getProgNumber` function, we need to modify the `internal/scanner/scanner_test.go` file.
    
    Here are the steps to add the test:
    
    1. Add a new test function `TestGetProgNumber` in the `scanner_test.go` file.
    2. In this function, define a set of test cases. Each test case should have a name, an input file name, and the expected prog number.
    3. For each test case, call the `getProgNumber` function with the input file name and check if the returned prog number matches the expected prog number.
    
    Here is the *edit block* for the changes:
    
    ```go
    internal/scanner/scanner_test.go
    <<<<<<< HEAD
    
    =======
    func TestGetProgNumber(t *testing.T) {
    	testCases := []struct {
    		name           string
    		input          string
    		expectedNumber int
    	}{
    		{
    			name:           "Prog 2000",
    			input:          "2000AD Prog 2000.pdf",
    			expectedNumber: 2000,
    		},
    		{
    			name:           "Prog 1234",
    			input:          "2000AD Prog 1234.pdf",
    			expectedNumber: 1234,
    		},
    		// Add more test cases here
    	}
    
    	for _, tc := range testCases {
    		t.Run(tc.name, func(t *testing.T) {
    			gotNumber := getProgNumber(tc.input, nil)
    			if gotNumber != tc.expectedNumber {
    				t.Errorf("getProgNumber(%v) = %v; want %v", tc.input, gotNumber, tc.expectedNumber)
    			}
    		})
    	}
    }
    >>>>>>> updated
    ```
    This test function will test the `getProgNumber` function with different file names and check if it correctly extracts the prog number from the file name.

--- internal/scanner/scanner_test.go
+++ internal/scanner/scanner_test.go
@@ -1,1 +1,29 @@
 package scanner
+func TestGetProgNumber(t *testing.T) {
+	testCases := []struct {
+		name           string
+		input          string
+		expectedNumber int
+	}{
+		{
+			name:           "Prog 2000",
+			input:          "2000AD Prog 2000.pdf",
+			expectedNumber: 2000,
+		},
+		{
+			name:           "Prog 1234",
+			input:          "2000AD Prog 1234.pdf",
+			expectedNumber: 1234,
+		},
+		// Add more test cases here
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			gotNumber := getProgNumber(tc.input, nil)
+			if gotNumber != tc.expectedNumber {
+				t.Errorf("getProgNumber(%v) = %v; want %v", tc.input, gotNumber, tc.expectedNumber)
+			}
+		})
+	}
+}
