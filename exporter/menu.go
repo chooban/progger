cbff6ec9fd09dcb104dd774d3a66a2be376bf033
Ross Hendry <rhendry@googlemail.com>
2024-04-23 20:49:08 +0100

    Start an about menu for OSS licenses

--- exporter/menu.go
+++ exporter/menu.go
@@ -2,13 +2,20 @@ package exporter
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
 	"github.com/chooban/progger/exporter/api"
 )
 
 func MainMenu(app *api.ProggerApp) *fyne.MainMenu {
-	mainMenu := fyne.NewMainMenu(fyne.NewMenu("Progger", fyne.NewMenuItem("Settings...", func() {
+	about := fyne.NewMenuItem("About", func() {
+		d := app.FyneApp.NewWindow("About")
+		d.SetContent(widget.NewLabel("This is about the app"))
+		d.Show()
+	})
+	settings := fyne.NewMenuItem("Settings...", func() {
 		app.AppContext.ShowSettings.Set(true)
-	})))
+	})
+	mainMenu := fyne.NewMainMenu(fyne.NewMenu("Progger", settings, about))
 
 	return mainMenu
 }
